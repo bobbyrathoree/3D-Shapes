76219b54916cc4e05903d05d9c66dfec6d2d4aaf
bobbyrathoree <bobbyprathore@gmail.com>
2019-10-06 02:40:38 -0400

    [ADD] generator network with 5 layers.

---
+++ generator.py
@@ -0,0 +1,35 @@
+from keras import Model
+from keras.layers import Input, Deconv3D, BatchNormalization, Activation
+
+
+def build_generator():
+    z_size = 200
+    filters = [512, 256, 128, 64, 1]
+    kernel_sizes = [4, 4, 4, 4, 4]
+    strides = [1, 2, 2, 2, 2]
+    input_shape = (1, 1, 1, z_size)
+    activations = ["relu", "relu", "relu", "relu", "sigmoid"]
+    convolutional_blocks = 5
+
+    input_layer = Input(shape=input_shape)
+
+    # First 3D deconvolution block
+    a = Deconv3D(
+        filters=filters[0], kernel_size=kernel_sizes[0], strides=strides[0]
+    )(input_layer)
+    a = BatchNormalization()(a, training=True)
+    a = Activation(activation=activations[0])(a)
+
+    # Next 4 will also be 3D deconvolution blocks
+    for x in range(convolutional_blocks - 1):
+        a = Deconv3D(
+            filters=filters[x + 1],
+            kernel_size=kernel_sizes[x + 1],
+            strides=strides[x + 1],
+        )(a)
+        a = BatchNormalization()(a, training=True)
+        a = Activation(activation=activations[x + 1])(a)
+
+    gen_model = Model(inputs=[input_layer], outputs=[a])
+
+    return gen_model
